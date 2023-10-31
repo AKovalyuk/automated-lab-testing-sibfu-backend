edc7b8f7528c778772100c916e2b56aad8d95719
Alex Kovalyuk <nehochubanan@gmail.com>
2023-10-31 20:19:01 +0700

    Add docker compose for code runners

---
+++ Makefile
@@ -0,0 +1,15 @@
+
+
+build_compilers:
+	docker build -t runner-compilers ./runner/compilers
+
+build_service:
+	docker build -t runner-service ./runner/service
+
+run:
+	make build_compilers
+	make build_service
+	docker compose up -d
+
+stop:
+	docker compose down
---
+++ runner/service/active.rb
@@ -0,0 +1,23 @@
+@languages ||= []
+@languages +=
+[
+  {
+    id: 46,
+    name: "Bash (5.0.0)",
+    is_archived: false,
+    source_file: "script.sh",
+    run_cmd: "/usr/local/bash-5.0/bin/bash script.sh"
+  },
+  {
+    id: 71,
+    name: "Python (3.10.1)",
+    is_archived: false,
+    source_file: "script.py",
+    run_cmd: "/usr/local/python-3.10.1/bin/python3 script.py"
+  },
+  {
+    id: 89,
+    name: "Multi-file program",
+    is_archived: false,
+  }
+]
